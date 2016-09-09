01ef2b34529a16be1bd94783fb04589bdc236403
ganshiren <wb-ganshiren@le.com>
2016-09-09 08:52:49 +0800

    Initial Commit

---
+++ UIWebviewBridgeSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIWebviewBridgeSample
+//
+//  Created by letv on 16/9/9.
+//  Copyright © 2016年 haha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ UIWebviewBridgeSampleTests/UIWebviewBridgeSampleTests.m
@@ -0,0 +1,39 @@
+//
+//  UIWebviewBridgeSampleTests.m
+//  UIWebviewBridgeSampleTests
+//
+//  Created by letv on 16/9/9.
+//  Copyright © 2016年 haha. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIWebviewBridgeSampleTests : XCTestCase
+
+@end
+
+@implementation UIWebviewBridgeSampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
